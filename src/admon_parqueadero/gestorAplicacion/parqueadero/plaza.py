3ebb8d69faca43ff00281aa9b7d3d7f56754c9e0
Sabelsl <141966602+Sabelsl@users.noreply.github.com>
2023-11-05 12:47:52 -0500

    Clase Plaza

--- src/admon_parqueadero/gestorAplicacion/parqueadero/plaza.py
+++ src/admon_parqueadero/gestorAplicacion/parqueadero/plaza.py
@@ -1,2 +1,55 @@
-class Plaza:
-    pass
+from gestorAplicacion.vehiculos import Vehiculo;
+import pickle
+plazasTotales  = []
+class Plaza (pickle):
+    numeroPlaza = 0
+    discapacitado = False #Si es una plaza para clientes discapacitados o no
+    estado = ""  #Disponible u ocupado
+    vehiculo = Vehiculo() # El vehículo que lo está ocupando
+    tipo = "" #Si es de tipo carro o moto
+    
+	# private static List<Plaza> plazasTotales = new ArrayList<Plaza>();
+	
+    def __init__(self,numeroPlaza,discapacitado,estado, vehi,  tipo):
+        self.numeroPlaza = numeroPlaza
+        self.discapacitado = discapacitado
+        self.estado = estado
+        self.vehiculo = vehi;
+        self.tipo = tipo;
+        plazasTotales.append(self);
+
+    def setDiscapacitado(self,disc):
+        self.discapacitado = disc
+
+    def getDiscapacitado(self):
+        return self.discapacitado
+
+
+    def setNumeroPlaza(self, numeroP) :
+        self.numeroPlaza = numeroP
+
+    def getNumeroPlaza(self) :
+        return self.numeroPlaza
+
+
+    def setEstado(self,estado) :
+        self.estado = estado
+
+    def getEstado(self) :
+        return self.estado
+
+
+    def setVehiculo(self,vehi) :
+        self.vehiculo = vehi
+
+
+    def getVehiculo(self) :
+        return self.vehiculo
+
+
+    def getTipo(self) :
+        return self.tipo
+
+
+    def setTipo(self,tipo) :
+        self.tipo = tipo
