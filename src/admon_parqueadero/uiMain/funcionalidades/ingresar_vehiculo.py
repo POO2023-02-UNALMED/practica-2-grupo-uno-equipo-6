12da0bd93d80ed70481285e57f4ca22a01bf49c9
sebasz-a <jpabuena@unal.edu.co>
2023-11-14 16:28:54 -0500

    cosas

---
+++ src/admon_parqueadero/uiMain/funcionalidades/ingresar_vehiculo.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from admon_parqueadero.uiMain.componentes.field_frame import FieldFrame
+
+from admon_parqueadero.uiMain.funcionalidades.base_funcionalidad import BaseFuncionalidad
+
+
+class IngresarVehiculo(BaseFuncionalidad):
+    def __init__(self, master: tk.Frame):
+        super().__init__(master)
+
+        self.setTitulo("Ingresar Vehiculo")
+        self.setDescripcion("descripcion")
+
+        contenido = tk.Frame(self)
+        self.setContenido(contenido)
+
+        field_frame = FieldFrame(contenido, "Hola", [], "Adios", None, None)
+        field_frame.pack()
+
+        frame_botones = tk.Frame(contenido)
+        frame_botones.pack()
+
+        btn1 = tk.Button(frame_botones, text="Hola")
+        btn1.pack()
--- src/admon_parqueadero/uiMain/ventanas/ventana_principal_usu.py
+++ src/admon_parqueadero/uiMain/ventanas/ventana_principal_usu.py
@@ -1,8 +1,11 @@
+from importlib.resources import as_file
 import tkinter as tk
 from tkinter import messagebox
 from typing import Any
 
 from admon_parqueadero.uiMain.componentes.label_ajustable import LabelAjustable
+from admon_parqueadero.uiMain.funcionalidades.ingresar_vehiculo import IngresarVehiculo
+from admon_parqueadero.uiMain.utils import ruta_imagen
 
 
 class ventana_principal_usu(tk.Frame):
@@ -26,12 +29,12 @@ class ventana_principal_usu(tk.Frame):
 
         menu_archivo.add_command(label="Aplicacion", command=self.aplicacion)
         menu_archivo.add_command(label="Salir", command=self.salir)
-        menu_procesos_consultas.add_command(label="Ingresar Vehiculo")
-        menu_procesos_consultas.add_command(label="Taller")
-        menu_procesos_consultas.add_command(label="Vender Carro")
-        menu_procesos_consultas.add_command(label="Comprar Carro")
-        menu_procesos_consultas.add_command(label="Manejo Parqueadero")
-        menu_procesos_consultas.add_command(label="Funcionalidades extras")
+        menu_procesos_consultas.add_command(label="Ingresar Vehiculo", command= lambda: self.cambiar_funcionalidad(IngresarVehiculo))
+        menu_procesos_consultas.add_command(label="Taller", command= lambda: self.cambiar_funcionalidad(IngresarVehiculo))
+        menu_procesos_consultas.add_command(label="Vender Carro", command= lambda: self.cambiar_funcionalidad(IngresarVehiculo))
+        menu_procesos_consultas.add_command(label="Comprar Carro", command= lambda: self.cambiar_funcionalidad(IngresarVehiculo))
+        menu_procesos_consultas.add_command(label="Manejo Parqueadero", command= lambda: self.cambiar_funcionalidad(IngresarVehiculo))
+        menu_procesos_consultas.add_command(label="Funcionalidades extras", command= lambda: self.cambiar_funcionalidad(IngresarVehiculo))
         menu_ayuda.add_command(label="Acerca de", command=self.acerca_de)
 
         self.master.config(menu=menu_bar)
@@ -56,15 +59,33 @@ class ventana_principal_usu(tk.Frame):
         )
 
     def interfaz_inicio(self) -> None:
-        interfaz_inicio = tk.Frame(
+        self.frame_funcionalidad = tk.Frame(
             self, highlightbackground="black", highlightthickness=2
         )
-        interfaz_inicio.pack(side="top", fill="both", expand=True, padx=10, pady=10)
+        self.frame_funcionalidad.pack(side="top", fill="both", expand=True, padx=10, pady=10)
 
         texto_bienvenida = tk.Label(
-            interfaz_inicio, text="Bienvenido", font=("Arial", 20)
+            self.frame_funcionalidad, text="Bienvenido", font=("Arial", 20)
         )
         texto_bienvenida.pack(anchor="n", padx=5, pady=5)
+        
+
+        # tratar de poner gif
+        frames_num = 80
+        archivo_ruta = ruta_imagen("guido-pitstop.gif")
+        with as_file(archivo_ruta) as archivo:
+            frames = [tk.PhotoImage(file=archivo, format='gif -index %i' %(i)) for i in range(frames_num)]
+            def update(ind):
+                """ Actualiza la imagen gif """
+                frame = frames[ind]
+                ind += 1
+                if ind == frames_num:
+                    ind = 0
+                canvas.create_image(0, 0, image=frame, anchor="nw")
+                self.frame_funcionalidad.after(80, update, ind) # Numero que regula la velocidad del gif
+            canvas = tk.Canvas(self.frame_funcionalidad, width=498, height=202)
+            canvas.pack(side="bottom", fill="x")
+            self.frame_funcionalidad.after(0, update, 0)
 
         texto = "Para empezar a usar la aplicación seleccione una de las opciones listadas en procesos y consultas, recomendamos empezar\
  ingresando un vehiculo que no se encuentre en el parqueadero. Esta aplicación le permitira administrar y realizar servicios\
@@ -72,6 +93,13 @@ class ventana_principal_usu(tk.Frame):
  ya que el programa se encargara del resto ;)"
 
         texto_descripcion = LabelAjustable(
-            interfaz_inicio, text=texto, font=("Arial", 12)
+            self.frame_funcionalidad, text=texto, font=("Arial", 12)
         )
         texto_descripcion.pack(padx=5, pady=10, fill="x")
+    
+    def cambiar_funcionalidad(self, clase_funcionalidad):
+        self.frame_funcionalidad.destroy()
+        self.frame_funcionalidad = tk.Frame(self)
+        self.frame_funcionalidad.pack()
+        funcionalidad = clase_funcionalidad(self.frame_funcionalidad)
+        funcionalidad.pack()
