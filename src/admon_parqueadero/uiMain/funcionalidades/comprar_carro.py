ecef11f2029d2b718e4e500472265f8415549d43
Sabelsl <141966602+Sabelsl@users.noreply.github.com>
2023-11-19 14:30:44 -0500

    comprarCarro

--- src/admon_parqueadero/uiMain/funcionalidades/comprar_carro.py
+++ src/admon_parqueadero/uiMain/funcionalidades/comprar_carro.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import tkinter as ttk
+ 
 from typing import Any
 from admon_parqueadero.baseDatos.baseDatos import BaseDatos
 from admon_parqueadero.uiMain.funcionalidades.base import BaseFuncionalidad
@@ -8,6 +8,8 @@ from admon_parqueadero.gestorAplicacion.personas.cliente import Cliente
 from admon_parqueadero.uiMain.componentes.field_frame import FieldFrame
 
 from admon_parqueadero.uiMain.componentes.formulario_cliente import FormularioCliente
+
+
 class ComprarCarro(BaseFuncionalidad):
     def __init__(
         self, master: tk.Misc, baseDatos: BaseDatos, *args: Any, **kwargs: Any
@@ -15,53 +17,130 @@ class ComprarCarro(BaseFuncionalidad):
         super().__init__(master, baseDatos, *args, **kwargs)
 
         self.setTitulo("Comprar Carro")
-        self.setDescripcion("Bienvenido, aquí podrá comprar un carro según sus preferencias.\
- Para comenzar, ingrese su documento, en caso de no estar registrado, \
- ingrese sus datos.")
+        self.setDescripcion("""Bienvenido, aquí podrá comprar un carro según sus preferencias.
+        Para comenzar, ingrese su documento, en caso de no estar registrado,
+        ingrese sus datos""")
         self.contenido = tk.Frame(self.frame_contenido)
         self.packContenido(self.contenido)
+
         formulario = FormularioCliente(
             self.contenido, self.getBaseDatos(), f_final=self._configurar_ui
         )
-        formulario.pack(anchor="s", fill="both", expand=True, ipadx=15, ipady=5)
-        
+        formulario.pack(anchor="s", fill="both", expand=True, ipadx=15, ipady=5)    
+
 
         
+        # self.all_vehiculo = []
+        # self.all_empleados = []
+        # for parqueadero in parqueaderos:
+        #     empleados = parqueadero.getEmpleados()
+        #     vehiculosVentaParqueadero = parqueadero.getVehiculosVenta()
+        #     self.all_vehiculo.extend(vehiculosVentaParqueadero)
+        #     self.all_empleados.extend(empleados)
+    
+    def filtrar (self):
+        self.listaCarros = []
+        seleccion = self.seleccion.get()
+        if seleccion == 1:
+            lista_aux = [vehiculo for vehiculo in self.all_vehiculo if vehiculo.isDiscapacitado() is True ]    
+        elif seleccion == 2:
+            lista_aux = [vehiculo for vehiculo in self.all_vehiculo if vehiculo.isDiscapacitado() is False ]
+        else :
+            lista_aux = [] 
+        for vehiculo in lista_aux:
+            color = vehiculo.getColor()
+            marca = vehiculo.getMarca()
+            valorVehiculo = vehiculo.getPrecioVenta()
+            
+            self.colorSelected = self.getValue('Color')
+            self.MarcaSelected = self.getValue('Marca')
+            self.precioMaximoInput = self.getValueNumero('Precio maximo')
+            if (valorVehiculo < self.precioMaximoInput 
+                and color == self.colorSelected 
+                    and marca == self.MarcaSelected) :
+                option=vehiculo.getPlaca() 
+                self.listaCarros.append(option)
+        self.listaCarros.append('No se encontraron registros') if len(self.listaCarros) == 0 else self.listaCarros
+       
+                            
+
+            
+        
+    def feedback():
+        pass
+        # 
     def _configurar_ui(self, cliente: Cliente) -> None:
+       
+        # mensaje de bienvenida
+        nombre = cliente.getNombre()
+        mensajeBienvenida = f"""Bienvenido de nuevo, {nombre}"""
         
-        # nombre = cliente.getNombre()
-        # f"""Bienvenido de nuevo, {nombre}"""
-        #! obtener una lista completa de todos los empleados de tipo Vendedor
-        # Empleado.getCargo()=='Vendedor'
+        self._vendedores_nombre = list(
+            map(lambda x: x.getNombre().title(), self._parqueadero.getVendedores())
+        )
+
+        self.all_vehiculo = [vehiculo for vehiculo in self._parqueadero.getVehiculosVenta() ]
         
-        # placas_vehiculos = map(lambda v: v.getPlaca(),cliente.getVehiculos() )
-        placas_vehiculos=['ejemplo1']
+        self._vehiculos_color = list(
+            map(lambda x: x.getColor().title(), self.all_vehiculo)
+        )
+        
+        self._vehiculos_marca = list(
+            map(lambda x: x.getMarca().title(), self.all_vehiculo)
+        )
         
         
-        # r marca, color, precio máximo y en caso de que el cliente sea discapacitado entonces se le
-        # dará una lista de carros que estarán previamente adaptados para especialmente personas discapacitadas.
-        lista_vendedores=['prueba1']
-        # selected_option = tk.StringVar()
-        # filtro_vehiculos = ttk.Combobox(window, values=options, textvariable=selected_option)
-        
-        # v = getVehiculos()
-        # v[0].getColor()
-        # v[0].getMarca()
-        # def precio_maximo (precioUsuario):
-        #     v[0].get
-        
-        # combobox = ttk.Combobox(self.contenido, values=lista_vendedores)
-        # combobox.pack()
         self.field_frame = FieldFrame(
             self.contenido,
-            "Criterio",
-            ["Vendedores", "Vehículo que desea comprar"],
-            "Valor",
-            [str(cliente.getCedula()), None],
-            ["Cédula"],
-            combobox={
-                "Vehículo que desea comprar": [*placas_vehiculos]
-            },
+            "Criterios",
+            ["Vendedor", "Color","Marca", "Precio maximo"],
+            "Valores",
+            [None,None, None, None],
+            [None, None, None, None],
+            combobox={"Vendedor": list(self._vendedores_nombre),
+                      
+                    "Color": list(self._vehiculos_color),
+                    "Marca":list(self._vehiculos_marca)
+                    }
+            ,titulo=mensajeBienvenida
         )
-        self.field_frame.pack(anchor="s", fill="both", expand=True, ipadx=15, ipady=5)
-        # self._generar_botones("Continuar")#, lambda: self._continuar_inicio(cliente))
+        self.field_frame.pack()
+        
+       
+        
+        etiqueta_personalizada = tk.Label(self.contenido, text="Desea un vehiculo adaptado para condicion de discapacidad")
+        etiqueta_personalizada.pack()
+        self.seleccion = tk.IntVar()  # Variable para almacenar la opción seleccionada
+
+        radio_btn1 = tk.Radiobutton(self.contenido, text="Si", variable=self.seleccion, value=1)
+        radio_btn1.pack()
+
+        radio_btn2 = tk.Radiobutton(self.contenido, text="No", variable=self.seleccion, value=2)
+        radio_btn2.pack()
+        
+        # self.precioMaximoInput=entrada.get()
+        # --discapacidad
+        # Boton Consultar 
+        
+    
+        BotonConsultar = tk.Button(self.contenido, text='Consultar',command=self.filtrar())
+        BotonConsultar.pack()
+        
+        
+        self.field_frame = FieldFrame(
+            self.contenido,
+            "",
+            ["lista"],
+            "",
+            valores=[None],
+            habilitado=[None],
+            combobox={"lista": self.listaCarros},
+            titulo='Lista de carros disponibles'
+        )
+        self.field_frame.pack()
+
+        
+
+
+
+
